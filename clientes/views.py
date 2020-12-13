2c76610ea2fbe7189bce8751aeda84ddfcd7d55c
Paulo <pauloestrella1994@gmail.com>
2020-12-13 15:36:15 -0300

    Examples of django class based views

--- clientes/views.py
+++ clientes/views.py
@@ -1,7 +1,13 @@
+from django.utils import timezone
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Person
 from .forms import PersonForm
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
 
 
 @login_required
@@ -41,3 +47,33 @@ def persons_delete(request, id):
         return redirect('person_list')
 
     return render(request, 'person_delete_confirm.html', {'person': person})
+
+
+class PersonList(ListView):
+    model = Person
+
+
+class PersonDetail(DetailView):
+    model = Person
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['now'] = timezone.now()
+        return context
+
+class PersonCreate(CreateView):
+    model = Person
+    fields = ['first_name', 'last_name', 'age', 'salary', 'bio', 'photo']
+    success_url = '/clientes/person_list/'
+
+class PersonUpdate(UpdateView):
+    model = Person
+    fields = ['first_name', 'last_name', 'age', 'salary', 'bio', 'photo']
+    success_url = reverse_lazy('person_list_cbv')
+
+class PersonDelete(DeleteView):
+    model = Person
+    #success_url = reverse_lazy('person_list_cbv')
+
+    def get_success_url(self):
+        return reverse_lazy('person_list_cbv')
--- home/views.py
+++ home/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
+from django.views.generic.base import TemplateView
+from django.http import HttpResponse
+from django.views import View
 
 
 def home(request):
@@ -9,3 +12,20 @@ def home(request):
 def my_logout(request):
     logout(request)
     return redirect('home')
+
+
+class HomePageView(TemplateView):
+    template_name = 'home2.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['minha_variavel'] = 'Ola, seja bem vindo ao curso de Django Advanced'
+        return context
+
+class MyView(View):
+    
+    def get(self, request, *args, **kwargs):
+        return render(request, 'home3.html')
+    
+    def post(self, request, *args, **kwargs):
+        return HttpResponse('Post')
